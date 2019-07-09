b1facf662b34606e7dab26ea935a055f58da7e15
dangus1924 <51095136+dangus1924@users.noreply.github.com>
2019-07-08 21:56:38 -0400

    explained  Implicit, New, and Expilicit Binding

--- assignments/this.js
+++ assignments/this.js
@@ -2,9 +2,9 @@
 * in your own words. explain the four principle for the "this" keyword below.
 *
 * 1. Window Binding is the Global context. It's when you call the function from the defualt window "consoloe.log(this)".
-* 2. Implicit Binding 
-* 3. 
-* 4. 
+* 2. Implicit Binding would be the object standing before the dot
+* 3. New Binding is when a object is created and referenced by "this" variable, inheriting the prototype of the function.
+* 4. Expiicit Binding is when you force a function call to use a certain object for "this" binding without putting a property function reference on the object.
 *
 * write out a code example of each explanation above
 */
